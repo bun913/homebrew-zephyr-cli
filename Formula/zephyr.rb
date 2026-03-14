60ee4a389efdbd6959e34f43da88dee73dcf7a7b
bun913 <nagasima.romance+@gmail.com>
2026-03-14 15:27:37 +0900

    chore: bump zephyr to v1.2.3

--- Formula/zephyr.rb
+++ Formula/zephyr.rb
@@ -1,23 +1,23 @@
 class Zephyr < Formula
   desc "CLI tool for Zephyr Scale API"
   homepage "https://github.com/bun913/zephyr-cli"
-  version "1.2.2"
+  version "1.2.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/bun913/zephyr-cli/releases/download/v#{version}/zephyr-macos-arm64"
-      sha256 "2a6ee0ed7d8c4ab31f2dca25d61b81b3508703eafbdef0d6d2a1dd2d2b884fa6"
+      sha256 "ec54b4a77c4ec86a5c065766f54e7ea7de4bf0b6f6d34ee68bfe3647d74888f2"
     end
     on_intel do
       url "https://github.com/bun913/zephyr-cli/releases/download/v#{version}/zephyr-macos-x64"
-      sha256 "68dfc60f043ca79294bd1f0bdcd9f49d28e797551761d84d4570fc00c9376151"
+      sha256 "f82735c46fc946a56ace3b67c863409e034ff64bdc07c685cdb632c4f7460d42"
     end
   end
 
   on_linux do
     url "https://github.com/bun913/zephyr-cli/releases/download/v#{version}/zephyr-linux-x64"
-    sha256 "7ecc04b0e81e5cb116791c7037267ad7b9368ba33cd454b1188bd3915f9880f9"
+    sha256 "9556496e389489727d9d2f667451688e384cd37bbe1cb9d961122d79dc788b76"
   end
 
   def install
